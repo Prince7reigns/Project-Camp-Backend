5cfd9f3d8e3ec40ca9bbf95df7cdd6c80c4d601f
Prince Yadav <prinsyadac@gmail.com>
2025-09-18 23:59:48 +0530

    feat: integrate bcrypt for password hashing and token generation in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose ,{Schema} from "mongoose"
-
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
@@ -61,6 +61,45 @@ const userSchema = new Schema(
         timestamps:true
     }
 )
+
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method("generateAccessToken",  function (){
+      return jwt.sign(
+        {
+           _id:this._id,
+           username:this.username,
+           email:this.email,
+           fullName:this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+      )
+   })
+
+   userSchema.method("generateRefreshToken" , function(){
+        return jwt.sign(
+            {
+             _id:this._id,
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+            }
+        )
+   })
+
 
 
 export const User = mongoose.model("Users",userSchema)
