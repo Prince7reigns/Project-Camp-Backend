56c4151bf9767ad10c53c64ef95c45cc27bb68e8
Prince Yadav <prinsyadac@gmail.com>
2025-09-19 23:31:56 +0530

    feat: add JWT token generation and temporary token functionality in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose ,{Schema} from "mongoose"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import crypto from "crypto"
 
 const userSchema = new Schema(
     {
@@ -72,34 +74,39 @@ userSchema.pre("save",async function(next){
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+};
 
-userSchema.method("generateAccessToken",  function (){
-      return jwt.sign(
-        {
-           _id:this._id,
-           username:this.username,
-           email:this.email,
-           fullName:this.fullName,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-        }
-      )
-   })
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+  );
+};
 
-   userSchema.method("generateRefreshToken" , function(){
-        return jwt.sign(
-            {
-             _id:this._id,
-            },
-            process.env.REFRESH_TOKEN_SECRET,
-            {
-                expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-            }
-        )
-   })
+userSchema.methods.generateTemporaryToken = function () {
+    const unHashedToken = crypto.randomBytes(20).toString("hex")
 
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex")
+
+    const tokenExpiry = Date.now() + (20*60*1000) //20 mins
+    return {unHashedToken, hashedToken, tokenExpiry}
+};
 
 
 export const User = mongoose.model("Users",userSchema)
